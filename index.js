3a8428884f6115d17a689355c6aee709613b89bc
Luis F. Hernandez <lhernandez@egifter.com>
2019-10-21 15:40:43 -0400

    Refactor to accommodate for command

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
-const regEx = /<span key=["*"][^>]*>/;
+const puppeteer = require('puppeteer');
+const {sanitizeUrl} = require('@braintree/sanitize-url');
+const fs = require('fs');
 
-function getTokens() {
-    const spans = document.getElementsByTagName('span');
-    const tokens = [];
+exports.getTokens = (async () => {
+    let url = sanitizeUrl(process.argv[2]);
 
-    for (let span of spans) {
-        if (span.attributes.key) {
-            tokens.push({
-                key: span.attributes.key.nodeValue,
-                value: span.attributes.key.ownerElement.textContent
-            });
+    console.log(`Getting tokens for: ${url}`);
+
+    await main(url);
+
+    async function main(url) {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        try {
+            await page.goto(url);
+            await page.waitFor(2000)
+
+            let tokens = await getTokens(page);
+
+
+            fs.writeFileSync('output.txt', JSON.stringify(tokens, null, 2));
+
+        } catch (error) {
+            console.error(error);
+        } finally {
+            browser.close();
         }
+
+
     }
 
-    return tokens;
-}
+    async function getTokens(page) {
+        return await page.evaluate(() => {
+            const spans = document.getElementsByTagName('span');
+            const tokens = [];
+
+            for (let span of spans) {
+                if (span.attributes.key) {
+                    tokens.push({
+                        key: span.attributes.key.nodeValue,
+                        value: span.attributes.key.ownerElement.textContent
+                    });
+                }
+            }
+
+            return tokens;
+        });
+    }
+})();
